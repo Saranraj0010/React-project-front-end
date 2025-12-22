d16b87a0f3191e02ddc95efa41cc848c2d44b218
Saranraj0010 <sraji20055@gmail.com>
2025-12-22 19:01:10 +0530

    first-project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import StudentRegisterForm from "./compounts/StudentRegisterForm";
+import StudentView from "./compounts/Studentview";
+
+
+const App = () =>{
+
+
+
+
+
+  return(
+    <>
+    <StudentRegisterForm/>
+    {/* <StudentView/> */}
+    </>
+  )
+}
+export default App;
---
+++ src/compounts/Studentview.jsx
@@ -0,0 +1,16 @@
+import StudentRegisterForm from "./StudentRegisterForm"
+const StudentView = (saran) => {
+
+
+    return (
+        <>
+            {/* <p>{saran.studentProfile.firstName}</p> */}
+            <p>hii</p>
+            {/* <StudentRegisterForm data={data}/> */}
+            {/* {saran.studentProfile.map((id) => (
+                <div key={id}>{saran.studentProfile.firstName}</div>
+            ))} */}
+        </>
+    )
+}
+export default StudentView
